e16082591fe53e45ab152c5036a97ad53e80b634
Emili García <egarcia@gisce.net>
2025-03-20 16:31:02 +0100

    Second try

--- facturae/utils.py
+++ facturae/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import pkcs12
 
 class FacturaeUtils(object):
@@ -14,8 +15,18 @@ class FacturaeUtils(object):
         assert passwd, "Passwd must be provided"
 
         try:
-            priv_key, cert, other_certs = pkcs12.load_key_and_certificates(
+            private_key, certificate, additional_certificates = pkcs12.load_key_and_certificates(
                 pk, passwd)
+
+            priv_key = private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            )
+
+            cert = certificate.public_bytes(
+                serialization.Encoding.PEM
+            )
 
             return priv_key, cert
 
